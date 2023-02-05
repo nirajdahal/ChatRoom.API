048e92078ca93d5b1388d4151b1344b1d9190dbc
DESKTOP-7LN1193\niraj <nirajdahal09@gmail.com>
2023-02-05 20:08:45 +1100

    added build command for typescript to not use absolute path

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Express } from 'express'
-import { ChatRoomServer } from './setupServer'
-import databaseConnection from './setupDatabase'
-import { config } from './config'
+import { ChatRoomServer } from '@root/setupServer'
+import databaseConnection from '@root/setupDatabase'
+import { config } from '@root/config'
 class Application {
     public initialize(): void {
         this.loadConfig()
